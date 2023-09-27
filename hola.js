cba058943485b0063eeae95cc52e2725284f5f5f
SergioJimenezMartin <146173110+SergioJimenezMartin@users.noreply.github.com>
2023-09-27 10:28:27 +0200

    Delete hola.js

--- hola.js
+++
@@ -1,9 +0,0 @@
-
-        function hola(){
-        alert("Hola ")
-        }
-        
-
-        document.getElementById("Terk").innerHTML = "Regod amigo"
-        document.getElementById("Cambiofoto").src = "giratinashiny.png"
-        document.getElementById("Cambiocolor").style.color = "grey"

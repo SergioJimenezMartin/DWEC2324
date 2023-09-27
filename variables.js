0d3f9434899f6fccb231fa088a2d1dc6a753abba
SergioJimenezMartin <146173110+SergioJimenezMartin@users.noreply.github.com>
2023-09-27 10:26:05 +0200

    Delete variables.js

--- variables.js
+++
@@ -1,6 +0,0 @@
-var primeraVariable= "Declarada fuera del bloque";
-function miFuncion() {
-    var primeraVariable="Declarada dentro de la función con el mismo nombre";
-    return 0; //se llama igual pero solo existe dentro de la función
-}
-Console.log(primeraVariable)

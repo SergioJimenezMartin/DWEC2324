b1237bd68c5cb0e72541f84de9be2f4a93c0f796
Sergio <manolo@gmail.com>
2023-09-27 10:00:32 +0200

    subimosfichero

---
+++ Ejemplos.js
@@ -0,0 +1,8 @@
+const STR1= 42 + "1";
+console.log(STR1);     //-> 421
+console.log(typeof STR1);   //->string
+
+
+const STR2= 42 - "1";
+console.log(STR2);     //-> 421
+console.log(typeof STR2);   //->string
